b391170d0ce8ef29b046a2379ba3ebe07ff18eb3
Radovan Semancik <radovan.semancik@evolveum.com>
2015-01-20 18:03:19 +0100

    Skeleton

---
+++ src/main/java/com/evolveum/polygon/LdapConnector.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015 Evolveum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolveum.polygon;
+
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.spi.Configuration;
+import org.identityconnectors.framework.spi.Connector;
+import org.identityconnectors.framework.spi.ConnectorClass;
+
+@ConnectorClass(displayNameKey = "ldap.connector.display", configurationClass = LdapConfiguration.class)
+public class LdapConnector implements Connector {
+
+    private static final Log LOG = Log.getLog(LdapConnector.class);
+
+    private LdapConfiguration configuration;
+    private LdapConnection connection;
+
+    @Override
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+
+    @Override
+    public void init(Configuration configuration) {
+        this.configuration = (LdapConfiguration)configuration;
+        this.connection = new LdapConnection(this.configuration);
+    }
+
+    @Override
+    public void dispose() {
+        configuration = null;
+        if (connection != null) {
+            connection.dispose();
+            connection = null;
+        }
+    }
+}
