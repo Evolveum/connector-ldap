d67c823ed6496ac08a6067cb2589e8e204e2959a
Radovan Semancik <radovan.semancik@evolveum.com>
2015-01-20 19:49:22 +0100

    Apache LDAP SDK dependency. Basic connection setup.

--- src/main/java/com/evolveum/polygon/LdapConnector.java
+++ src/main/java/com/evolveum/polygon/LdapConnector.java
@@ -16,18 +16,28 @@
 
 package com.evolveum.polygon;
 
+import java.io.IOException;
+
+import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.ldap.client.api.LdapConnectionConfig;
+import org.apache.directory.ldap.client.api.LdapNetworkConnection;
 import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.common.security.GuardedString;
+import org.identityconnectors.framework.common.exceptions.ConnectorException;
+import org.identityconnectors.framework.common.exceptions.ConnectorIOException;
 import org.identityconnectors.framework.spi.Configuration;
 import org.identityconnectors.framework.spi.Connector;
 import org.identityconnectors.framework.spi.ConnectorClass;
+import org.identityconnectors.framework.spi.PoolableConnector;
+import org.identityconnectors.framework.spi.operations.TestOp;
 
 @ConnectorClass(displayNameKey = "ldap.connector.display", configurationClass = LdapConfiguration.class)
-public class LdapConnector implements Connector {
+public class LdapConnector implements PoolableConnector, TestOp {
 
     private static final Log LOG = Log.getLog(LdapConnector.class);
 
     private LdapConfiguration configuration;
-    private LdapConnection connection;
+    private LdapNetworkConnection connection;
 
     @Override
     public Configuration getConfiguration() {
@@ -37,15 +47,64 @@ public class LdapConnector implements Connector {
     @Override
     public void init(Configuration configuration) {
         this.configuration = (LdapConfiguration)configuration;
-        this.connection = new LdapConnection(this.configuration);
+        connect();
     }
-
+    
     @Override
+	public void test() {
+    	checkAlive();
+		// TODO
+	}
+
+    // TODO: methods
+    
+    
+    private void connect() {
+    	final LdapConnectionConfig connectionConfig = new LdapConnectionConfig();
+    	connectionConfig.setLdapHost(configuration.getHost());
+    	connectionConfig.setLdapPort(configuration.getPort());
+    	connectionConfig.setName(configuration.getBindDn());
+    	
+    	GuardedString bindPassword = configuration.getBindPassword();
+    	if (bindPassword != null) {
+    		// I hate this GuardedString!
+    		bindPassword.access(new GuardedString.Accessor() {
+				@Override
+				public void access(char[] chars) {
+					connectionConfig.setCredentials(new String(chars));
+				}
+			});
+    	}
+    	
+		connection = new LdapNetworkConnection(connectionConfig);
+		try {
+			boolean connected = connection.connect();
+			if (!connected) {
+				throw new ConnectorIOException("Unable to connect to LDAP server "+configuration.getHost()+":"+configuration.getPort()+" due to unknown reasons");
+			}
+		} catch (LdapException e) {
+			throw new ConnectorIOException("Unable to connect to LDAP server "+configuration.getHost()+":"+configuration.getPort()+": "+e.getMessage(), e);
+		}
+    }
+    
+    @Override
+	public void checkAlive() {
+		if (!connection.isConnected()) {
+			throw new ConnectorException("Connection check failed");
+		}
+	}
+
+	@Override
     public void dispose() {
         configuration = null;
         if (connection != null) {
-            connection.dispose();
+        	try {
+				connection.close();
+			} catch (IOException e) {
+				throw new ConnectorIOException(e.getMessage(), e);
+			}
             connection = null;
         }
     }
+
 }
