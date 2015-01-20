30019f00fbaa9839edb52f89cfa6cbe0a2845166
Radovan Semancik <radovan.semancik@evolveum.com>
2015-01-20 21:22:23 +0100

    Rudimentary schema implementation

--- src/main/java/com/evolveum/polygon/LdapConnector.java
+++ src/main/java/com/evolveum/polygon/LdapConnector.java
@@ -17,24 +17,35 @@
 package com.evolveum.polygon;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.directory.api.ldap.model.exception.LdapException;
+import org.apache.directory.api.ldap.model.schema.AttributeType;
+import org.apache.directory.api.ldap.model.schema.LdapSyntax;
+import org.apache.directory.api.ldap.model.schema.SchemaManager;
 import org.apache.directory.ldap.client.api.LdapConnectionConfig;
 import org.apache.directory.ldap.client.api.LdapNetworkConnection;
 import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.common.security.GuardedString;
 import org.identityconnectors.framework.common.exceptions.ConnectorException;
 import org.identityconnectors.framework.common.exceptions.ConnectorIOException;
+import org.identityconnectors.framework.common.objects.AttributeInfoBuilder;
+import org.identityconnectors.framework.common.objects.ObjectClassInfoBuilder;
+import org.identityconnectors.framework.common.objects.Schema;
+import org.identityconnectors.framework.common.objects.SchemaBuilder;
 import org.identityconnectors.framework.spi.Configuration;
-import org.identityconnectors.framework.spi.Connector;
 import org.identityconnectors.framework.spi.ConnectorClass;
 import org.identityconnectors.framework.spi.PoolableConnector;
+import org.identityconnectors.framework.spi.operations.SchemaOp;
 import org.identityconnectors.framework.spi.operations.TestOp;
 
 @ConnectorClass(displayNameKey = "ldap.connector.display", configurationClass = LdapConfiguration.class)
-public class LdapConnector implements PoolableConnector, TestOp {
+public class LdapConnector implements PoolableConnector, TestOp, SchemaOp {
 
     private static final Log LOG = Log.getLog(LdapConnector.class);
+    private static final Map<String, Class<?>> SYNTAX_MAP = new HashMap<String,Class<?>>(); 
 
     private LdapConfiguration configuration;
     private LdapNetworkConnection connection;
@@ -56,10 +67,63 @@ public class LdapConnector implements PoolableConnector, TestOp {
 		// TODO
 	}
 
-    // TODO: methods
+    @Override
+	public Schema schema() {
+		try {
+			connection.loadSchema();
+		} catch (LdapException e) {
+			throw new ConnectorIOException(e.getMessage(), e);
+		}
+		SchemaManager schemaManager = connection.getSchemaManager();
+		SchemaBuilder schemaBuilder = new SchemaBuilder(LdapConnector.class);
+		for (org.apache.directory.api.ldap.model.schema.ObjectClass ldapObjectClass: schemaManager.getObjectClassRegistry()) {
+			ObjectClassInfoBuilder ocib = new ObjectClassInfoBuilder();
+			ocib.setType(ldapObjectClass.getName());
+			addAttributeTypes(ocib, ldapObjectClass.getMustAttributeTypes(), true);
+			addAttributeTypes(ocib, ldapObjectClass.getMayAttributeTypes(), false);
+			schemaBuilder.defineObjectClass(ocib.build());
+		}
+		return schemaBuilder.build();
+	}
     
     
-    private void connect() {
+    private Class<?> toIcfType(LdapSyntax syntax) {
+    	Class<?> type = SYNTAX_MAP.get(syntax.getName());
+    	if (type == null) {
+    		LOG.warn("No type mapping for syntax {0}, using string", syntax.getName());
+    		return String.class;
+    	} else {
+    		return type;
+    	}
+	}
+
+	private void addAttributeTypes(ObjectClassInfoBuilder ocib, List<AttributeType> attributeTypes, boolean isRequired) {
+		for (AttributeType ldapAttribute: attributeTypes) {
+			AttributeInfoBuilder aib = new AttributeInfoBuilder(ldapAttribute.getName());
+			aib.setRequired(isRequired);
+			aib.setType(toIcfType(ldapAttribute.getSyntax()));
+			if (ldapAttribute.isOperational()) {
+				aib.setReturnedByDefault(false);
+			}
+			if (ldapAttribute.isSingleValued()) {
+				aib.setMultiValued(false);
+			} else {
+				aib.setMultiValued(true);
+			}
+			if (ldapAttribute.isReadOnly()) {
+				aib.setCreateable(false);
+				aib.setUpdateable(false);
+			}
+			ocib.addAttributeInfo(aib.build());
+		}
+	}
+
+	// TODO: methods
+    
+    
+    
+
+	private void connect() {
     	final LdapConnectionConfig connectionConfig = new LdapConnectionConfig();
     	connectionConfig.setLdapHost(configuration.getHost());
     	connectionConfig.setLdapPort(configuration.getPort());
@@ -106,5 +170,9 @@ public class LdapConnector implements PoolableConnector, TestOp {
             connection = null;
         }
     }
+	
+	static {
+		SYNTAX_MAP.put("TODO",String.class);
+	}
 
 }
