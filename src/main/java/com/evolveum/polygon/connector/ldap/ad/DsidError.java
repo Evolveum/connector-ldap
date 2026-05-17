c6a153b0be4f6a77b9320e1ac556f50320ff0c31
Sylinsic <38617929+Sylinsic@users.noreply.github.com>
2026-05-17 16:07:54 +0100

    feat: Add DSID-0C090D10 as an X_BIND_REQUIRED error requiring reconnect

--- src/main/java/com/evolveum/polygon/connector/ldap/ad/DsidError.java
+++ src/main/java/com/evolveum/polygon/connector/ldap/ad/DsidError.java
@@ -32,7 +32,8 @@ public class DsidError {
     // There are three codes for "X_BIND_REQUIRED" error. Strange, yet not entirely surprising.
     static public final String CODE_X_BIND_REQUIRED_1 =  "0C0907E9";
     static public final String CODE_X_BIND_REQUIRED_2 =  "0C090A71";
-    static public final String CODE_X_BIND_REQUIRED_3 =  "0C090C88";
+    static public final String CODE_X_BIND_REQUIRED_3 = "0C090C88";
+    static public final String CODE_X_BIND_REQUIRED_4 = "0C090D10";
 
     static public final String CODE_UNAVAILABLE_CRITICAL_EXTENSION =  "0C090850";
 
@@ -98,7 +99,14 @@ public class DsidError {
                 // Looks like something on the server has "logged out" the connection, while LDAP channel remains active.
                 //
                 // MID-9724
-                return new DsidError(CODE_X_BIND_REQUIRED_3, "Connection was unbound on the server", diagnosticMessage, ReconnectException.class);
+                return new DsidError(CODE_X_BIND_REQUIRED_3, "Connection was unbound on the server", diagnosticMessage,
+                        ReconnectException.class);
+
+            case CODE_X_BIND_REQUIRED_4:
+                // 000004DC: LdapErr: DSID-0C090D10, comment: In order to perform this operation a successful bind must be completed on the connection., data 0, v4f7c?: X_BIND_REQUIRED: In order to perform this operation a successful bind must be completed on the connection
+                //
+                // Looks like something on the server has "logged out" the connection, while LDAP channel remains active.
+                return new DsidError(CODE_X_BIND_REQUIRED_4, "Connection was unbound on the server", diagnosticMessage, ReconnectException.class);
 
             case CODE_UNAVAILABLE_CRITICAL_EXTENSION:
                 // unavailableCriticalExtension: 00000057: LdapErr: DSID-0C090850, comment: Error processing control, data 0, v2580? (12)
